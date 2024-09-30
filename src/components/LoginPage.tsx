c1feea5da87b3f43fbc1c1a68d11c8660cd0378f
imohammedz <mohdzabir4969@gmail.com>
2024-09-30 12:48:04 -0700

    Updating signup button

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -53,7 +53,14 @@ const LoginPage: React.FC = () => {
           />
         </div>
         <Link to="/signup" className="header-link">
-          <span>Sign Up</span>
+          <span>
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+              Signup
+            </button>
+          </span>
         </Link>
         <button type="submit">Login</button>
       </form>
